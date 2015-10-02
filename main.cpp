52c9e9a289eceafde5795c9d3e32aa3cd24f5ae7
MateuszMateo <k.mateusz120@gmail.com>
2015-10-02 13:54:35 +0200

    Próba wprowadzenia funkcji
    Nieudana próba wprowadzenia funkcji w nowym kodzie

--- main.cpp
+++ main.cpp
@@ -1,44 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+
+ bool funkcja(long long int n;){ //coś tu jest źle zrobione...
+
+    long long silnia = 1;
+    for (int i=n;i>1;i--){
+        silnia = silnia * i;}
+    return silnia;
+}
+
 int main()
 {
-    long long int n1,n2,n3,n4,wynik;
-      long long silnia1 = 1;
-      long long silnia2 = 1;
-      long long silnia3 = 1;
-      long long silnia4 = 1;
-
-      cout<<"Podaj wartosci:";
-      cin>>n1>>n2>>n3;
-
-      for(int i1=n1;i1>1;i1--){
-        silnia1 = silnia1%1000000007 * i1;}
-
-      for(int i2=n2;i2>1;i2--){
-        silnia2 = silnia2%1000000007 * i2;}
-
-      for(int i3=n3;i3>1;i3--){
-        silnia3 = silnia3%1000000007 * i3;}
-
-      n4 = n1 + n2 + n3;
-
-      for(int i4=n4;i4>1;i4--){
-        silnia4 = silnia4%1000000007 * i4;}
-
-      silnia4 = silnia4%1000000007;
-      silnia3 = silnia3%1000000007;
-      silnia2 = silnia2%1000000007;
-      silnia1 = silnia1%1000000007;
-
-      wynik = (silnia4 / (silnia1 * silnia2 * silnia3)%1000000007);
-
-      cout << wynik << endl;
+    long long int D1, D2, D3;
+    cout<<"Podaj wartosci:"<<endl;
+    cin>>D1>>D2>>D3;
 
 
-
-
-
+    return 0;
 }
 
